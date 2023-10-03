42c1203f168ba22ad2f0af72992d26ad97174d57
Marcos Vinicius <101298473+viinii3@users.noreply.github.com>
2023-10-03 17:30:21 -0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+
+function compilaSass(){
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+function comprimeJS(){
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'));
+}
+
+function comprimeImagem(){
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'))
+}
+
+exports.sass = compilaSass;
+exports.javascript = comprimeJS;
+exports.images = comprimeImagem;
+
+exports.default = function(){
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJS));
+    gulp.watch('./source/images/*', {ignoreInitial: false}, gulp.series(comprimeImagem));
+}
