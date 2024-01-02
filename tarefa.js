684e3542bde4757d25a238557735201d7bae23ef
Marcos Vinicius <101298473+viinii3@users.noreply.github.com>
2024-01-02 14:31:38 -0300

    Add files via upload

---
+++ tarefa.js
@@ -0,0 +1,12 @@
+"use strict";
+let num1 = 5;
+let num2 = 10;
+function multiplicarNumeros(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicarNumeros(num1, num2));
+let nome = "vini";
+function saudaçoes(nome) {
+    return "Olá " + nome;
+}
+console.log(saudaçoes(nome));
---
+++ tarefa.ts
@@ -0,0 +1,16 @@
+
+let num1: number = 5;
+let num2: number = 10;
+
+function multiplicarNumeros(num1: number, num2: number): number{
+    return num1 * num2;
+}
+console.log(multiplicarNumeros(num1,num2));
+
+
+
+let nome: string = "vini";
+function saudaçoes(nome: string){
+    return "Olá " + nome
+}
+console.log(saudaçoes(nome))
